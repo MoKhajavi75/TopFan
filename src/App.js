19adc1ba37b41371d31ced9efff62a213c6b5d35
MohamadKh75 <khajavi_mohamad@yahoo.com>
2019-07-21 11:59:57 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,71 +1,72 @@
 import React, { Component } from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 
-class BreweryDetailScreen extends Component {
-  constructor(props) {
-    super(props);
+class App extends Component {
+  constructor() {
+    super();
 
     this.state = {
-      isLoading: true,
-      data: []
+      backColor: 'white'
     };
   }
 
-  componentWillMount() {
-    this.getBrewDeetsHandler();
-  }
-
-  getBrewDeetsHandler = () => {
-    fetch('api url', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        itemID: this.props.navigation.state.params.id
-      })
-    })
-      .then(res => res.json())
-      .then(myJson => {
-        this.setState({
-          isLoading: false,
-          data: myJson
-        });
-        console.log(this.state.data[0].venueName);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  };
-
   render() {
-    if (this.state.isLoading) {
-      return (
+    let buttons = [1, 2, 3];
+
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: 5
+        }}
+      >
         <View
           style={{
             flex: 1,
+            flexDirection: 'row',
             justifyContent: 'center',
-            alignItems: 'center'
+            alignItems: 'center',
+            alignSelf: 'stretch',
+            backgroundColor: 'rgb(245,246,247)',
+            borderWidth: 1,
+            paddingRight: 80
           }}
         >
-          <ActivityIndicator size='large' color='red' />
+          {buttons.map(item => {
+            return (
+              <View
+                key={item}
+                style={{
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderWidth: 1
+                }}
+              >
+                <Text style={{ fontSize: 32 }}>{item * 2}</Text>
+              </View>
+            );
+          })}
         </View>
-      );
-    } else {
-      return (
+
         <View
           style={{
-            flex: 1,
+            flex: 9,
             justifyContent: 'center',
-            alignItems: 'center'
+            alignItems: 'center',
+            alignSelf: 'stretch',
+            backgroundColor: 'rgb(225,234,245)',
+            borderWidth: 1
           }}
         >
-          <Text>{this.state.data[0].venueName}</Text>
+          <Text style={{ fontSize: 32 }}>Bottom</Text>
         </View>
-      );
-    }
+      </View>
+    );
   }
 }
 
-export default BreweryDetailScreen;
+export default App;
