dd330add09490253221128f546cca46fab9b07d3
MohamadKh75 <khajavi_mohamad@yahoo.com>
2019-07-25 15:08:50 +0430

    Add more sections

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { View, Text, TouchableOpacity } from 'react-native';
 
 class App extends Component {
   render() {
+    const buttons = ['View', 'Edit', 'Help'];
+
     return (
       <View
         style={{
@@ -23,63 +25,69 @@ class App extends Component {
             backgroundColor: 'white'
           }}
         >
-          <TouchableOpacity
-            style={{
-              justifyContent: 'center',
-              alignItems: 'center',
-              alignSelf: 'stretch'
-            }}
-            onPress={() => {
-              alert('View');
-            }}
-          >
-            <Text
-              style={{ marginHorizontal: 25, fontSize: 20, color: 'black' }}
-            >
-              View
-            </Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={{
-              justifyContent: 'center',
-              alignItems: 'center',
-              alignSelf: 'stretch'
-            }}
-          >
-            <Text
-              style={{ marginHorizontal: 25, fontSize: 20, color: 'black' }}
-            >
-              Edit
-            </Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={{
-              justifyContent: 'center',
-              alignItems: 'center',
-              alignSelf: 'stretch'
-            }}
-          >
-            <Text
-              style={{ marginHorizontal: 25, fontSize: 20, color: 'black' }}
-            >
-              Help
-            </Text>
-          </TouchableOpacity>
+          {buttons.map(item => {
+            return (
+              <TouchableOpacity
+                key={item}
+                style={{
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  alignSelf: 'stretch'
+                }}
+                onPress={() => {
+                  alert(item);
+                }}
+              >
+                <Text
+                  style={{ marginHorizontal: 25, fontSize: 20, color: 'black' }}
+                >
+                  {item}
+                </Text>
+              </TouchableOpacity>
+            );
+          })}
         </View>
 
-        {/* Buttons */}
+        {/* Bottom section */}
         <View
           style={{
             flex: 8,
-            flexDirection: 'row',
+            flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
             alignSelf: 'stretch',
             backgroundColor: '#E6E6FA'
           }}
-        />
+        >
+          {/* Output */}
+          <View
+            style={{
+              flex: 2,
+              justifyContent: 'flex-end',
+              alignItems: 'flex-end',
+              alignSelf: 'stretch',
+              backgroundColor: '#D3D3D3',
+              margin: 20,
+              borderWidth: 1
+            }}
+          >
+            <Text style={{ fontSize: 52, margin: 5 }}>0</Text>
+          </View>
+
+          {/* Buttons */}
+          <View
+            style={{
+              flex: 8,
+              justifyContent: 'center',
+              alignItems: 'center',
+              alignSelf: 'stretch',
+              backgroundColor: 'white',
+              margin: 20
+            }}
+          >
+            <Text style={{ fontSize: 52 }}>Buttons</Text>
+          </View>
+        </View>
       </View>
     );
   }
