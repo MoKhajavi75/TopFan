24bf5c30258225a850856acd6d221f098d17ed16
MohamadKh75 <khajavi_mohamad@yahoo.com>
2019-08-01 14:10:13 +0430

    Finish Calculator

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
-import App from './src/HW6';
+import App from './src/HW1';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- src/HW1.js
+++ src/HW1.js
@@ -5,6 +5,13 @@ class HW1 extends Component {
   render() {
     const buttons = ['View', 'Edit', 'Help'];
 
+    const rows = [
+      ['MC', 'MR', 'MS', 'M+', 'M-'],
+      ['<-', 'CE', 'C', '+-', '√'],
+      ['7', '8', '9', '/', '%'],
+      ['4', '5', '6', 'x', '1/x']
+    ];
+
     return (
       <View
         style={{
@@ -82,10 +89,184 @@ class HW1 extends Component {
               alignItems: 'center',
               alignSelf: 'stretch',
               backgroundColor: 'white',
-              margin: 20
+              margin: 20,
+              padding: 10,
+              borderWidth: 1
             }}
           >
-            <Text style={{ fontSize: 52 }}>Buttons</Text>
+            {rows.map((eachRow, rowIndex) => {
+              return (
+                <View
+                  key={rowIndex}
+                  style={{
+                    flex: 1,
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    alignSelf: 'stretch',
+                    borderWidth: 1
+                  }}
+                >
+                  {eachRow.map((button, buttonIndex) => {
+                    return (
+                      <View
+                        key={buttonIndex}
+                        style={{
+                          flex: 1,
+                          flexDirection: 'row',
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                          alignSelf: 'stretch',
+                          borderWidth: 1
+                        }}
+                      >
+                        <Text style={{ fontSize: 30 }}>{button}</Text>
+                      </View>
+                    );
+                  })}
+                </View>
+              );
+            })}
+
+            {/* last row */}
+            <View
+              style={{
+                flex: 2,
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                alignSelf: 'stretch',
+                borderWidth: 1
+              }}
+            >
+              <View
+                style={{
+                  flex: 4,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  alignSelf: 'stretch',
+                  borderWidth: 1
+                }}
+              >
+                <View
+                  style={{
+                    flex: 1,
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    alignSelf: 'stretch',
+                    borderWidth: 1
+                  }}
+                >
+                  <View
+                    style={{
+                      flex: 1,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      alignSelf: 'stretch',
+                      borderWidth: 1
+                    }}
+                  >
+                    <Text>1</Text>
+                  </View>
+
+                  <View
+                    style={{
+                      flex: 1,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      alignSelf: 'stretch',
+                      borderWidth: 1
+                    }}
+                  >
+                    <Text>2</Text>
+                  </View>
+
+                  <View
+                    style={{
+                      flex: 1,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      alignSelf: 'stretch',
+                      borderWidth: 1
+                    }}
+                  >
+                    <Text>3</Text>
+                  </View>
+
+                  <View
+                    style={{
+                      flex: 1,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      alignSelf: 'stretch',
+                      borderWidth: 1
+                    }}
+                  >
+                    <Text>-</Text>
+                  </View>
+                </View>
+
+                <View
+                  style={{
+                    flex: 1,
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    alignSelf: 'stretch',
+                    borderWidth: 1
+                  }}
+                >
+                  <View
+                    style={{
+                      flex: 2,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      alignSelf: 'stretch',
+                      borderWidth: 1
+                    }}
+                  >
+                    <Text>0</Text>
+                  </View>
+
+                  <View
+                    style={{
+                      flex: 1,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      alignSelf: 'stretch',
+                      borderWidth: 1
+                    }}
+                  >
+                    <Text>.</Text>
+                  </View>
+
+                  <View
+                    style={{
+                      flex: 1,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      alignSelf: 'stretch',
+                      borderWidth: 1
+                    }}
+                  >
+                    <Text>+</Text>
+                  </View>
+                </View>
+              </View>
+
+              <View
+                style={{
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  alignSelf: 'stretch',
+                  borderWidth: 1
+                }}
+              >
+                <Text>=</Text>
+              </View>
+            </View>
           </View>
         </View>
       </View>
