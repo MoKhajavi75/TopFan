58f8f0aa53a8fe4340cdc582e1a170d6f5ac9166
MohamadKh75 <khajavi_mohamad@yahoo.com>
2019-08-08 14:18:50 +0430

    Create styles.js

---
+++ src/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  full: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  }
+});
+
+export default styles;
