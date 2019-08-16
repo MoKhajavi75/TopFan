77f6f788fd5219a12a1d34de5ca4d308219805bd
MohamadKh75 <khajavi_mohamad@yahoo.com>
2019-08-17 01:46:04 +0430

    Create Container.js

---
+++ src/components/Container.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Container extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#708090',
+          padding: 10
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            alignSelf: 'stretch'
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 40,
+              color: 'white',
+              fontWeight: 'bold'
+            }}
+          >
+            {this.props.title}
+          </Text>
+        </View>
+
+        {this.props.children}
+      </View>
+    );
+  }
+}
+
+export { Container };
