aa97f2ad2a6acaa50af055fb5b6a4aba5d61e999
MohamadKh75 <khajavi_mohamad@yahoo.com>
2019-08-17 01:47:10 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { YellowBox } from 'react-native';
 import Router from './Router';
+
+YellowBox.ignoreWarnings(['Method `jumpToIndex` is']);
 
 class App extends Component {
   render() {
