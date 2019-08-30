4a4eac8da1a7ba68166666e0bb33ec811ef7231d
MohamadKh75 <khajavi_mohamad@yahoo.com>
2019-08-30 12:41:28 +0430

    Update Currency
    Use SectionList

--- src/components/CurrencyCard.js
+++ src/components/CurrencyCard.js
@@ -1,64 +1,123 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text } from 'react-native';
 
 class CurrencyCard extends Component {
   render() {
-    return (
-      <View
-        style={{
-          height: 130,
-          width: 350,
-          flexDirection: 'row-reverse',
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderRadius: 20,
-          borderWidth: 3,
-          borderColor: 'black',
-          backgroundColor: 'white',
-          marginVertical: 15
-        }}
-      >
+    if (this.props.type == 1) {
+      return (
         <View
           style={{
-            flex: 1.2,
+            height: 130,
+            width: 350,
+            flexDirection: 'row-reverse',
             justifyContent: 'center',
             alignItems: 'center',
-            alignSelf: 'stretch',
-            borderLeftWidth: 3
-          }}
-        >
-          <Image style={{ width: 70, height: 70 }} source={this.props.image} />
-        </View>
-
-        <View
-          style={{
-            flex: 3,
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            alignSelf: 'stretch'
+            borderRadius: 20,
+            borderWidth: 3,
+            borderColor: 'black',
+            backgroundColor: 'white',
+            marginVertical: 10
           }}
         >
           <View
             style={{
-              flex: 1,
+              flex: 1.2,
               justifyContent: 'center',
               alignItems: 'center',
               alignSelf: 'stretch',
-              marginBottom: 10
+              borderLeftWidth: 3
             }}
           >
-            <Text style={{ fontSize: 20, color: 'black', marginBottom: 20 }}>
-              {'خرید: ' + this.props.buy}
-            </Text>
-
-            <Text style={{ fontSize: 20, color: 'black' }}>
-              {'فروش: ' + this.props.sell}
+            <Text style={{ fontSize: 20, color: 'black', textAlign: 'center' }}>
+              {this.props.code}
             </Text>
           </View>
+
+          <View
+            style={{
+              flex: 3,
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              alignSelf: 'stretch'
+            }}
+          >
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                alignSelf: 'stretch',
+                marginBottom: 10
+              }}
+            >
+              <Text style={{ fontSize: 20, color: 'black', marginBottom: 20 }}>
+                {'خرید: ' + this.props.buy}
+              </Text>
+
+              <Text style={{ fontSize: 20, color: 'black' }}>
+                {'فروش: ' + this.props.sell}
+              </Text>
+            </View>
+          </View>
         </View>
-      </View>
-    );
+      );
+    } else {
+      return (
+        <View
+          style={{
+            height: 130,
+            width: 350,
+            flexDirection: 'row-reverse',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 20,
+            borderWidth: 3,
+            borderColor: 'black',
+            backgroundColor: 'white',
+            marginVertical: 10
+          }}
+        >
+          <View
+            style={{
+              flex: 1.2,
+              justifyContent: 'center',
+              alignItems: 'center',
+              alignSelf: 'stretch',
+              borderLeftWidth: 3
+            }}
+          >
+            <Text style={{ fontSize: 20, color: 'black', textAlign: 'center' }}>
+              {this.props.code}
+            </Text>
+          </View>
+
+          <View
+            style={{
+              flex: 3,
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              alignSelf: 'stretch'
+            }}
+          >
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                alignSelf: 'stretch',
+                marginBottom: 10
+              }}
+            >
+              <Text style={{ fontSize: 20, color: 'black' }}>
+                {'نرخ: ' + this.props.rate}
+              </Text>
+            </View>
+          </View>
+        </View>
+      );
+    }
   }
 }
 
--- src/screens/Currency.js
+++ src/screens/Currency.js
@@ -1,7 +1,123 @@
 import React, { Component } from 'react';
+import { View, Text, SectionList } from 'react-native';
+import Axios from 'axios';
+import { CurrencyCard, Spinner } from '../components';
+
+class Currency extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: true,
+      data: {}
+    };
+  }
+
+  componentDidMount() {
+    const url = 'https://currency.jafari.pw/json';
+
+    Axios.get(url)
+      .then(response => {
+        this.setState({ data: response.data, isLoading: false });
+      })
+      .catch(error => {
+        this.setState({ isLoading: false });
+        alert(error);
+      });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <Spinner />;
+    } else {
+      const header = Object.keys(this.state.data);
+
+      return (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#708090',
+            padding: 10
+          }}
+        >
+          <View
+            style={{
+              flex: 8,
+              justifyContent: 'center',
+              alignItems: 'center',
+              alignSelf: 'stretch'
+            }}
+          >
+            <SectionList
+              style={{ flex: 1 }}
+              renderSectionHeader={({ section: { title } }) => (
+                <View
+                  style={{
+                    height: 80,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    alignSelf: 'stretch',
+                    borderRadius: 10,
+                    backgroundColor: 'khaki'
+                  }}
+                >
+                  <Text style={{ fontSize: 28, fontWeight: 'bold' }}>
+                    {title}
+                  </Text>
+                </View>
+              )}
+              sections={[
+                {
+                  title: header[1],
+                  data: this.state.data[header[1]],
+                  renderItem: ({ item }) => (
+                    <CurrencyCard
+                      type={1}
+                      code={item.Currency}
+                      sell={item.Sell}
+                      buy={item.Buy}
+                    />
+                  )
+                },
+                {
+                  title: header[2],
+                  data: this.state.data[header[2]],
+                  renderItem: ({ item }) => (
+                    <CurrencyCard
+                      type={1}
+                      code={item.Coin}
+                      sell={item.Sell}
+                      buy={item.Buy}
+                    />
+                  )
+                },
+                {
+                  title: header[3],
+                  data: this.state.data[header[3]],
+                  renderItem: ({ item }) => (
+                    <CurrencyCard type={2} code={item.Name} rate={item.Rate} />
+                  )
+                }
+              ]}
+              keyExtractor={(item, index) => item + index}
+              showsVerticalScrollIndicator={false}
+            />
+          </View>
+        </View>
+      );
+    }
+  }
+}
+
+export { Currency };
+
+/*
+import React, { Component } from 'react';
 import { View, FlatList } from 'react-native';
 import Axios from 'axios';
-import { CurrencyCard, Spinner, Container } from '../components';
+import { CurrencyCard, Spinner } from '../components';
 
 class Currency extends Component {
   constructor() {
@@ -105,3 +221,4 @@ class Currency extends Component {
 }
 
 export { Currency };
+*/
