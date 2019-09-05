d6d7b25c40e02778f39986105ed0d6a28b9647da
MohamadKh75 <khajavi_mohamad@yahoo.com>
2019-09-05 10:36:03 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import { YellowBox } from 'react-native';
 import Router from './Router';
 
-YellowBox.ignoreWarnings(['Method `jumpToIndex` is']);
+YellowBox.ignoreWarnings([
+  'Method `jumpToIndex` is',
+  'createTabNavigator is deprecated'
+]);
 
 class App extends Component {
   render() {
