a0393d6c939f839d64a7670fbf5332affdeca09e
MohamadKh75 <khajavi_mohamad@yahoo.com>
2019-09-27 12:56:43 +0330

    Add global strings

---
+++ src/globals/Strings.js
@@ -0,0 +1,46 @@
+const fa = {
+  DIRECTION: 'rtl',
+  header_text: 'یادآور',
+  settings_locale: 'زبان برنامه',
+  settings_theme: 'پوسته برنامه',
+  yellow: 'زرد',
+  coral: 'مرجانی',
+  red: 'قرمز',
+  confirm: 'تایید',
+  inputPlaceHolder: 'عنوان...',
+  deleteTitle: 'حذف',
+  deleteMessage: 'مطمئنی؟',
+  deleteYes: 'آره',
+  deleteNo: 'نه بابا!'
+};
+
+const en = {
+  DIRECTION: 'ltr',
+  header_text: 'To Do',
+  settings_locale: 'Language',
+  settings_theme: 'Theme',
+  yellow: 'Yellow',
+  coral: 'Coral',
+  red: 'Red',
+  confirm: 'Confirm',
+  inputPlaceHolder: 'title...',
+  deleteTitle: 'Delete',
+  deleteMessage: 'Are you sure?',
+  deleteYes: 'Yep',
+  deleteNo: 'Nope!'
+};
+
+const strings = locale => {
+  switch (locale) {
+    case 'fa':
+      return fa;
+
+    case 'en':
+      return en;
+
+    default:
+      return en;
+  }
+};
+
+export { strings };
--- src/globals/index.js
+++ src/globals/index.js
@@ -1,1 +1,2 @@
 export * from './Colors.js';
+export * from './Strings.js';
