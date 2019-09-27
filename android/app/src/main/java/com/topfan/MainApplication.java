70991ebf90261344c9523ebbd918c88afbc31c4d
MohamadKh75 <khajavi_mohamad@yahoo.com>
2019-09-27 16:29:56 +0330

    Cleanups!

--- android/app/src/main/java/com/topfan/MainApplication.java
+++ android/app/src/main/java/com/topfan/MainApplication.java
@@ -3,7 +3,7 @@ package com.topfan;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-
+import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,7 +24,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNCameraPackage()
       );
     }
 
--- src/App.js
+++ src/App.js
@@ -1,51 +1,13 @@
-import React, { Component } from 'reactn';
-import { AsyncStorage, ActivityIndicator, View } from 'react-native';
+import React, { Component } from 'react';
 import Router from './Router';
 
 class App extends Component {
   constructor() {
     super();
-
-    this.state = {
-      isLoading: true
-    };
-
-    this.initiate();
-  }
-
-  initiate() {
-    AsyncStorage.getItem('preferences')
-      .then(preferences => {
-        let parsed = JSON.parse(preferences);
-
-        if (parsed) {
-          this.setGlobal({
-            locale: parsed.locale,
-            theme: parsed.theme
-          });
-        } else {
-          this.setGlobal({
-            locale: 'en',
-            theme: 'coral'
-          });
-        }
-      })
-      .catch(error => console.warn(error))
-      .finally(() => this.setState({ isLoading: false }));
   }
 
   render() {
-    if (this.state.isLoading) {
-      return (
-        <View
-          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-        >
-          <ActivityIndicator size='large' />
-        </View>
-      );
-    } else {
-      return <Router />;
-    }
+    return <Router />;
   }
 }
 
