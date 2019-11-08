fdba23233442672517e47875ade09927df659807
MohamadKh75 <khajavi_mohamad@yahoo.com>
2019-11-08 15:27:17 +0330

    Cleanups!

--- src/Router.js
+++ src/Router.js
@@ -1,39 +1,11 @@
-import { createStackNavigator, createSwitchNavigator } from 'react-navigation';
-import { Splash, Login, Landing } from './screens';
-
-const AuthStack = createStackNavigator(
-  {
-    _Login: Login
-  },
-  {
-    initialRouteName: '_Login',
-    navigationOptions: {
-      header: null
-    }
-  }
-);
 
 const RootStack = createStackNavigator(
   {
-    _Landing: Landing
   },
   {
-    initialRouteName: '_Landing',
     navigationOptions: {
       header: null
     }
   }
 );
 
-const SwitchNavigator = createSwitchNavigator(
-  {
-    _Splash: Splash,
-    _AuthStack: AuthStack,
-    _RootStack: RootStack
-  },
-  {
-    initialRouteName: '_Splash'
-  }
-);
-
-export default SwitchNavigator;
