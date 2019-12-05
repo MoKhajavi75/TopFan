4f3a898f65f5107c72ba4615cfdba8e1c8267009
MohamadKh75 <khajavi_mohamad@yahoo.com>
2019-12-05 15:42:05 +0330

    Add an example

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -5,10 +5,23 @@ class Login extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      table: {
+        hasan: 'rohani',
+        divar: '22',
+        ajor: '3cm',
+        غذا: 'قرمه سبزی',
+        rang: 'coral',
+        قیمت: '22.000'
+      }
+    };
   }
 
   render() {
+    const price = this.state.table['قیمت']
+      ? this.state.table['قیمت']
+      : undefined;
+
     return (
       <View
         style={{
@@ -19,7 +32,53 @@ class Login extends Component {
           padding: 10
         }}
       >
-        <Text style={{ fontSize: 25, color: 'white' }}>Login</Text>
+        {Object.entries(this.state.table).map((pair, index) => (
+          <View
+            key={index.toString()}
+            style={{
+              //flex: 1,
+              width: 300,
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: index % 2 ? '#AAA' : '#EEE',
+              borderWidth: 1
+            }}
+          >
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                alignSelf: 'stretch',
+                borderRightWidth: 1
+              }}
+            >
+              <Text
+                style={{ fontSize: 22, fontWeight: 'bold', marginVertical: 10 }}
+              >{`${pair[0]}`}</Text>
+            </View>
+
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                alignSelf: 'stretch'
+              }}
+            >
+              <Text
+                style={{ fontSize: 20, marginVertical: 10 }}
+              >{`${pair[1]}`}</Text>
+            </View>
+          </View>
+        ))}
+
+        {price ? (
+          <Text
+            style={{ fontSize: 26, color: 'white', marginTop: 20 }}
+          >{`قیمت: ${price}`}</Text>
+        ) : null}
       </View>
     );
   }
